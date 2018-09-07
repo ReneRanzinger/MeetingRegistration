876570e11f07e2227b7d42658cc05fce370d8a59
vinamrjain <er.vinamrajain@gmail.com>
2018-09-07 11:17:54 -0400

    updated bash script

--- execute.sh
+++ execute.sh
@@ -2,23 +2,27 @@
 
 PIDFile="registrationApplication.pid"
 
-echo "Finding existing running application"
+echo "Finding existing running application."
 if [ ! -f $PIDFile ]
 then
+echo "No running instance of application found. Creating runningApplication.pid file to store the PID of the next process."
 touch registrationApplication.pid
 else
 cleanup
 fi
 
+echo "Running the new instance of application"
 mvn -U -Djasypt.encryptor.password=$JASYPT_SECRET -DskipTests=true spring-boot:run > log &
 Application_PID=$!
 echo $Application_PID > $PIDFile
+echo "Done! Application is now up and running"
 
 function cleanup(){
 old_PID=$(<"$PIDFile")
 if ps -p $old_PID > /dev/null
 then
-kill -9 $old_pid
+	echo "Killing the existing instance of application"
+	kill -9 $old_pid
 fi
 }
 
